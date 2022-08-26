b2536a87357dd2faddf8d2c4c2398efc50b741eb
Anusha <sharmaanusha384@gmail.com>
2022-08-26 16:30:50 +0530

    commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 //New Quotes
 function newQuote() {
     //Pick a random Quote
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
@@ -12,7 +21,7 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
+        console.log(apiQuotes[]);
     } catch(error) {
         //Catch Error Here
     }
